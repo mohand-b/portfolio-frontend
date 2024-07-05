860ff9106cd169713d24c44b4ccf1ddd8119d7f4
mohand-b <mohandbdj@gmail.com>
2024-07-05 14:16:59 +0200

    chore: ignore environment configuration files

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-const baseUrl = 'https://portfolio-mohandb-45ef0333502c.herokuapp.com';
-
-export const environment = {
-  production: true,
-  baseUrl: baseUrl,
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-const baseUrl = 'http://localhost:3000';
-
-export const environment = {
-  production: false,
-  baseUrl: baseUrl,
-};
